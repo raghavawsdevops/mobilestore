0b157af7e8c4bd9c188d4726234c5a3128dcffb3
cicd <cicd@email.com>
2021-10-20 23:33:10 +0530

    maven-groupid-changes

--- src/main/java/com/mobilestore/MobileStoreApplication.java
+++ src/main/java/com/mobilestore/MobileStoreApplication.java
@@ -1,4 +1,6 @@
-public class HelloWorld {
+package com.mobilestore;
+
+public class MobileStoreApplication {
     public static void main(String[] args) {
         System.out.println("Hello Capstone Project");
     }
