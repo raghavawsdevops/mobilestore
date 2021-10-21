ef2093e515e1db104375f5f56b0f76f7549fe3d3
cicd <cicd@email.com>
2021-10-21 13:30:04 +0530

    login-controller

--- src/main/java/com/mobilestore/controller/MobileLoginController.java
+++ src/main/java/com/mobilestore/controller/MobileLoginController.java
@@ -1,16 +1,16 @@
 package com.mobilestore.controller;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import com.mobilestore.model.MobileUser;
+import com.mobilestore.service.MobileService;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-import com.mobilestore.model.MobileUser;
-import com.mobilestore.service.MobileService;
 
 public class MobileLoginController extends HttpServlet {
 
